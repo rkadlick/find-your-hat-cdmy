5c851544515eabd00a6bc4bf2b58830334ad8409
rkadlick <27776470+rkadlick@users.noreply.github.com>
2024-05-27 18:44:11 -0400

    comments, README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Codecademy Find Your Hat Project
+
+## Table of Contents
+* [Project Goals](#project-goals)
+* [Built With](#built-with)
+* [How to Use](#how-to-use)
+* [Credits](#credits)
+
+## Project Goals
+1. **Project Context**
+    * In this project, you’ll be building an interactive terminal game. The scenario is that the player has lost their hat in a field full of holes, and they must navigate back to it without falling down one of the holes or stepping outside of the field.
+2. **Project Objectives**
+    * Using node.js build a maze game, played through the terminal
+	* Field is generated and validated using DFS to ensure maze is completable.
+
+## Built With
+* Javascript, NodeJS
+
+## How to Use
+1. make sure [NodeJS](https://nodejs.org/en/download/package-manager) is installed
+2. download `main.js`
+3. use the terminal to navigate to the folder containing `main.js`
+4. add the prompt-sync package, `npm i prompt-sync`
+5. run the game `node main.js`
+
+## Credits
+This was a practice project from the Back End Engineer path at [Codecademy](https://www.codecademy.com)
--- main.js
+++ main.js
@@ -6,15 +6,18 @@ class Field {
 	  this.playerPosition = {x: 0, y: 0};
 	}
   
+	// print field on terminal for user
 	print(){
 	  for(let row of this.field){
 		console.log(row.join(''))
 	  }
 	}
   
+	// move players (*) direction
 	movePlayer(direction){
 	  let {x, y} = this.playerPosition;
   
+	  // U for up, D for down, L for left, R for right
 	  switch (direction) {
 		case 'U':
 		  y -= 1;
@@ -32,6 +35,7 @@ class Field {
 		  console.log('Invalid input! Use U, D, L, R for directions.');
 		  return;
 	  }
+	  // checks if player moved out of boundary boxes
 	  if(this.isOutOfBounds(x,y)){
 		console.log('You moved out of bounds. Game Over!')
 		process.exit()
@@ -48,10 +52,12 @@ class Field {
 	  }
 	}
   
+	// established boundary in relation to field size
 	isOutOfBounds(x,y){
 	  return x < 0 || y < 0 || x >= this.field[0].length || y >= this.field[1].length
 	}
 
+	// begins the game, prompts the user
 	playGame(){
 		while(true){
 			myField.print();
@@ -60,30 +66,35 @@ class Field {
 		}
 	}
 
+	// generate field, place the holes (O) and hat (^) in random places
 	static generateField(height, width, percentage = 0.2){
 		const field = Array.from({ length: height }, () => Array.from({ length: width }, () => '░'));
 
 		const totalCells = height*width;
 		const numberOfHoles = Math.floor(totalCells * percentage)
 
+		// create number of holes based on percentage number
 		for(let i = 0; i < numberOfHoles; i++){
 			let x, y;
 			do {
 				x = Math.floor(Math.random() * width)
 				y = Math.floor(Math.random() * height)
-			} while(field[y][x] !== '░' && (x === 0 && y === 0));
+			} while(field[y][x] !== '░' && (x === 0 && y === 0)); // hole can not appear on player start
 			field[y][x] = 'O'
 		}
 
+		// place hat in random spot
 		let hatX, hatY;
 		do {
 			hatX = Math.floor(Math.random() * width);
 			hatY = Math.floor(Math.random() * height);
-		} while (field[hatY][hatX] !== '░' && (hatY === 0 && hatX == 0));
+		} while (field[hatY][hatX] !== '░' && (hatY === 0 && hatX == 0)); // hat can not appear on player start
 		field[hatY][hatX] = '^'
 
+		// place player
 		field[0][0] = '*'
 
+		// validate field
 		if(this.validate(field)){
 			return field
 		} else {
@@ -91,27 +102,39 @@ class Field {
 		}
 	}
 
+	// uses dfs search to validate if the maze can be completed
 	static validate(field){
 		const height = field.length;
 		const width = field[0].length;
+		// create array to keep track of visisted cells
 		const visited = Array.from({ length: height }, () => Array(width).fill(false));
 
+		// dfs search
 		function dfs(x, y) {
+			// check if current position is out of bounds, a hole, or visited
 			if (x < 0 || y < 0 || x >= width || y >= height || field[y][x] === 'O' || visited[y][x]) {
 			  return false;
 			}
+			// chck if user found hat
 			if (field[y][x] === '^') {
 			  return true;
 			}
+			// mark space as visited
 			visited[y][x] = true;
+			// explore all possible directions
 			return dfs(x + 1, y) || dfs(x - 1, y) || dfs(x, y + 1) || dfs(x, y - 1);
 		}
 	  
+		// start dfs at starting place
 		return dfs(0, 0);	
 	}
 }
 
+// generate field of size 5x5
+// change numbers to change field size
+
 const field = Field.generateField(5,5)
 const myField = new Field (field)
 
+// start game
 myField.playGame();
